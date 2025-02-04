96299dd2e1415ed65caa858ce113e18012a301ba
vladimir12312 <122517246+vladimir12312@users.noreply.github.com>
2025-02-04 14:26:49 +0000

    .

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
+vakances=[{
+    "title" : "1",
+    "text" : "2"
+    },{
+    "title":"3",
+    "text":"4"
+    }]
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", vakances=vakances)
 
 @app.route("/about")
 def about():
