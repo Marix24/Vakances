08cef58903c5c75ffc3204f58205864b8d19a504
vladimir12312 <122517246+vladimir12312@users.noreply.github.com>
2025-02-11 14:33:21 +0000

    add request to gov

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template, request
-
+import requests
 app = Flask(__name__)
+import requests
+reqjson={}
+vakances=[]
+text=requests.get(url="https://data.gov.lv/dati/lv/api/3/action/datastore_search?resource_id=7f68f6fc-a0f9-4c31-b43c-770e97a06fda", data=reqjson)
+text=text.json()
+results=text["result"]
+for i in range (len(results)):
 
-vakances=[{
-    "title" : "1",
-    "text" : "2",
-    "link":"https://www.w3schools.com"
-    },{
-    "title":"3",
-    "text":"4",
-    "link":"https://www.w3schools.com"
-    }]
+    title=results["records"][i]["Vakances nosaukums"]
+    link=results["records"][i]["Vakances paplašināts apraksts"]
+    text=results["records"][i]["Vakances kategorija"]
+    newelement={
+            "title":title,
+            "link":link,
+            "text": text,
+        }
+    vakances.append(newelement)
 @app.route("/")
 def index():
     return render_template("index.html", vakances=vakances)
