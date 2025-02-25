9b2aa0b7c14fb173815945a8f3fb681bd5ca3b34
Marix24 <122208599+Marix24@users.noreply.github.com>
2025-02-25 14:34:06 +0000

    datubaze saglaba datus

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 import sqlite3, requests
 sql_create = """
     CREATE TABLE IF NOT EXISTS vakances (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
         vakancesnr INTEGER,
         aktdatums TEXT,
         regnr TEXT,
@@ -19,60 +18,89 @@ sql_create = """
     );
 """
 sql_store = """
-    INSERT INTO vakances (id, vakancesnr, aktdatums, regnr, nosaukums, kategorija, algano, algalidz, slodze, darbalaiks, termins, attels, vieta, apraksts)
-    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?);
+    INSERT INTO vakances (vakancesnr, aktdatums, regnr, nosaukums, kategorija, algano, algalidz, slodze, darbalaiks, termins, attels, vieta, apraksts)
+    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);
 """
-def create_db():
+sql_get_data = """
+    SELECT * FROM vakances;
+"""
+conn = None
+cursor = None
+def open_db():
+    global conn, cursor
     conn = sqlite3.connect("data.db")
     cursor = conn.cursor()
-    cursor.execute(sql_create)
+
+def close_db():
+    global conn, cursor
     conn.commit()
     conn.close()
+    conn = None
+    cursor = None
+def create_db():
+    global conn, cursor
+    cursor.execute(sql_create)
+
 def fetch_and_store_data():
     response = requests.get("https://data.gov.lv/dati/lv/api/3/action/datastore_search?resource_id=7f68f6fc-a0f9-4c31-b43c-770e97a06fda")
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM punkti")
     data = response.json()
     #print( data )
     rows = data["result"]["records"]
-    conn = sqlite3.connect("data.db")
-    cursor.execute("DELETE FROM punkti")
+    cursor.execute("DELETE FROM vakances")
 
     for row in rows:
         #print( (row['NMPP_KODS'], row['NMPP_NOSAUKUMS'], row['NMPP_ADRESE']) )
-        cursor.execute( sql_store, (row["_id"], row["Vakances Nr"], row["Aktualizācijas datums"], row["Iestādes reģistrācijas numurs"], row["Vakances nosaukums"], row["Vakances kategorija"], , row["Alga no"], row["Alga līdz"], row["Slodzes tips"], row["Darba laika veids"], row["Pieteikšanās termiņš"], row["Attēls"], row["Vieta"], row["Vakances paplašināts apraksts"]) )
-    conn.commit()
-    conn.close()
+        cursor.execute(sql_store , (row["Vakances Nr"], row["Aktualizācijas datums"], row["Iestādes reģistrācijas numurs"], row["Vakances nosaukums"], row["Vakances kategorija"], row["Alga no"], row["Alga līdz"], row["Slodzes tips"], row["Darba laika veids"], row["Pieteikšanās termiņš"], row["Attēls"], row["Vieta"], row["Vakances paplašināts apraksts"]))
+def get_data( filter ):
+    open_db()
+    create_db()
+    #create_tables()
+    fetch_and_store_data() #saglabā jaunāko datu versiju no API uz db
+
+    cursor.execute( sql_get_data )
+    rows = cursor.fetchall()
+
+    close_db()
+    return rows
+
     
 
 app = Flask(__name__)
-import requests
-reqjson={}
-vakances=[]
-text=requests.get(url="https://data.gov.lv/dati/lv/api/3/action/datastore_search?resource_id=7f68f6fc-a0f9-4c31-b43c-770e97a06fda", data=reqjson)
-text=text.json()
-results=text["result"]
-for i in range (len(results)):
-
-    title=results["records"][i]["Vakances nosaukums"]
-    link=results["records"][i]["Vakances paplašināts apraksts"]
-    text=results["records"][i]["Vakances kategorija"]
-    newelement={
-            "title":title,
-            "link":link,
-            "text": text,
-        }
-    vakances.append(newelement)
 @app.route("/")
 def index():
-    return render_template("index.html", vakances=vakances)
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
     return render_template("about.html")
-@app.route("/test")
-def test():
-    print(ok)
-    create_db()
-    fetch_and_store_data()
+@app.route("/filter", methods = ['GET'])
+def filter(): 
+    filter = request.args.get('filter', default="")
+    rows = get_data(filter)
+    
+    
+    
+    print(rows)
+    return render_template("filter.html", rows = rows)
+#@app.route("/test")
+#import requests
+#reqjson={}
+#vakances=[]
+#text=requests.get(url="https://data.gov.lv/dati/lv/api/3/action/datastore_search?resource_id=7f68f6fc-a0f9-4c31-b43c-770e97a06fda", data=reqjson)
+#text=text.json()
+#results=text["result"]
+#for i in range (len(results)):
+#    title=results["records"][i]["Vakances nosaukums"]
+#    link=results["records"][i]["Vakances paplašināts apraksts"]
+#    text=results["records"][i]["Vakances kategorija"]
+#    newelement={
+#            "title":title,
+#            "link":link,
+#            "text": text,
+#        }
+#    vakances.append(newelement)
+#
+#def test():
+#    print(ok)
+#    create_db()
+#    fetch_and_store_data()
