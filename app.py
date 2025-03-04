ad4da8eae82148c408b5a96aca75eb0807cdb268
Marix24 <122208599+Marix24@users.noreply.github.com>
2025-03-04 14:01:16 +0000

    nonemu nost komentarus

--- app.py
+++ app.py
@@ -82,25 +82,3 @@ def filter():
     
     print(rows)
     return render_template("filter.html", rows = rows)
-#@app.route("/test")
-#import requests
-#reqjson={}
-#vakances=[]
-#text=requests.get(url="https://data.gov.lv/dati/lv/api/3/action/datastore_search?resource_id=7f68f6fc-a0f9-4c31-b43c-770e97a06fda", data=reqjson)
-#text=text.json()
-#results=text["result"]
-#for i in range (len(results)):
-#    title=results["records"][i]["Vakances nosaukums"]
-#    link=results["records"][i]["Vakances paplašināts apraksts"]
-#    text=results["records"][i]["Vakances kategorija"]
-#    newelement={
-#            "title":title,
-#            "link":link,
-#            "text": text,
-#        }
-#    vakances.append(newelement)
-#
-#def test():
-#    print(ok)
-#    create_db()
-#    fetch_and_store_data()
